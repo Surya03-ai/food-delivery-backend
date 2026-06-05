2417d9400e0431700071a63cd213e99ecef39d61
Akhileshelukapelli <elukapelliakhilesh@gmail.com>
2026-06-05 14:44:04 +0530

    Initial Spring Boot backend setup

---
+++ src/main/java/com/fooddeliveryapp/demo/FoodDeliveryBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.fooddeliveryapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fooddeliveryapp/demo/FoodDeliveryBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooddeliveryapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
